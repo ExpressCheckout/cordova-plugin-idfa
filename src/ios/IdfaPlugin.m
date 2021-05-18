58de4b629ae01c192afccbbe0831d35cb60517ad
machty <machty@gmail.com>
2021-05-18 14:43:12 -0400

    Add support for AppTrackingTransparency
    Upstream commit:
    
    https://github.com/chemerisuk/cordova-plugin-idfa/commit/75997e79b3a39e6aea998e07840795ed31414093

--- src/ios/IdfaPlugin.m
+++ src/ios/IdfaPlugin.m
@@ -1,6 +1,7 @@
 #import "IdfaPlugin.h"
 #import "UICKeyChainStore.h"
 #import <AdSupport/ASIdentifierManager.h>
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
 
 @implementation IdfaPlugin
 
@@ -10,6 +11,8 @@
         NSString *idfaString = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         NSString *uuidUserDefaults = [defaults objectForKey:@"uuid"];
+
+        NSDictionary* resultData;
         
         NSString *uuid = [UICKeyChainStore stringForKey:@"uuid"];
         if (!uuid) {
@@ -27,12 +30,46 @@
         [defaults synchronize];
         [UICKeyChainStore setString:uuid forKey:@"uuid"];
 
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{
-            @"idfa": idfaString,
-            @"uuid": uuid,
-            @"limitAdTracking": [NSNumber numberWithBool:!adidEnabled]
-        }];
+
+        if (@available(iOS 14, *)) {
+            NSNumber* trackingPermission = @(ATTrackingManager.trackingAuthorizationStatus);
+            // workaround, as long as Apple deferred the roll-out of manadatory tracking permission popup
+            // we'll assume that if the idfa string is not nullish, then it's allowed by user
+            if (!adidEnabled && uuid != nil && ![@"00000000-0000-0000-0000-000000000000" isEqualToString:uuid]) {
+                adidEnabled = YES;
+            }
+
+            resultData = @{
+                @"idfa": uuid,
+                @"trackingLimited": [NSNumber numberWithBool:!adidEnabled],
+                @"trackingPermission": trackingPermission
+            };
+        } else {
+            resultData = @{
+                @"idfa": uuid,
+                @"trackingLimited": [NSNumber numberWithBool:!adidEnabled]
+            };
+        }
+
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultData];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)requestPermission:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        if (@available(iOS 14, *)) {
+            [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+                CDVPluginResult* pluginResult =
+                    [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsNSUInteger:status];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }];
+        } else {
+            CDVPluginResult* pluginResult = [CDVPluginResult
+                                             resultWithStatus:CDVCommandStatus_ERROR
+                                             messageAsString:@"requestPermission is supported only for iOS >= 14"];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
     }];
 }
 
